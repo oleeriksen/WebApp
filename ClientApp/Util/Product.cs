c73a1112c66e27e21f9f7741a28609f37062fbbc
oleeriksen <oleeriksen1958@gmail.com>
2024-03-14 12:42:24 +0100

    forms mm

---
+++ ClientApp/Util/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClientApp.Util
+{
+    public class Product
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Name must be less than 50 characters.")]
+        public string? Name { get; set; }
+
+        [Required]
+        [Range(1,int.MaxValue, ErrorMessage = "Price must be positive")]
+        public int? Price { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateTime PublishedDate { get; set; } = DateTime.Now;
+
+        
+    }
+}
+
