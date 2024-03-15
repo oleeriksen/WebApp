35d47de7c6403ee99083781d33bf6846a1fb35c1
oleeriksen <oleeriksen1958@gmail.com>
2024-03-15 11:00:30 +0100

    todo listen

--- ClientApp/Util/ToDoItem.cs
+++ ClientApp/Util/ToDoItem.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+using System.Collections;
+
 namespace ClientApp.Util
 {
-    public class ToDoItem
+    public class ToDoItem : IComparable<ToDoItem>
     {
         public bool IsDone { get; set; }
         public required string Title { get; set; }
+
+      
+
+        public int CompareTo(ToDoItem? other)
+        {
+            return IsDone.CompareTo(other.IsDone);
+        }
     }
 }
 
