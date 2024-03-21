787431d2555318f33d3920e39c9a484c16a03b76
oleeriksen <oleeriksen1958@gmail.com>
2024-03-21 14:17:52 +0100

    init

---
+++ ServerAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServerAPI.Controllers;
+
+[ApiController]
+[Route("vejr")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    [HttpGet]
+    [Route("{n}")]
+    public IEnumerable<WeatherForecast> Get(int n = 5)
+    {
+        return Enumerable.Range(1, n).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
+
