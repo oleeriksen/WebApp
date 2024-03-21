2c3de025723ad711eba6383b6df1b2dafc435188
oleeriksen <oleeriksen1958@gmail.com>
2024-03-21 13:39:26 +0100

    clean-up

--- ClientApp/Util/ToDoItem.cs
+++ ClientApp/Util/ToDoItem.cs
@@ -3,16 +3,16 @@ using System.Collections;
 
 namespace ClientApp.Util
 {
-    public class ToDoItem : IComparable<ToDoItem>
+    public class ToDoItem //: IComparable<ToDoItem>
     {
         public bool IsDone { get; set; }
         public required string Title { get; set; }
         public int Priority { get; set; } = 2;
 
-        public int CompareTo(ToDoItem? other)
-        {
-            return -Priority.CompareTo(other.Priority);
-        }
+        //public int CompareTo(ToDoItem? other)
+        //{
+        //    return -Priority.CompareTo(other.Priority);
+        //}
     }
 }
 
